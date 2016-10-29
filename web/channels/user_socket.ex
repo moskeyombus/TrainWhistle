64d87e56ca206d3da4cf613724122421a4673848
John Wnek <john.wnek@callrail.com>
2016-10-28 22:11:21 -0400

    initial Heroku config

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule TrainWhistle.UserSocket do
   # channel "room:*", TrainWhistle.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
