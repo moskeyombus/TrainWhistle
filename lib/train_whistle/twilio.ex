906a74bbd689e5942f44dd04ef34772e2935ad51
serri588 <samara.duncan@innovativearchitects.com>
2016-10-29 08:28:43 -0400

    initial functions

--- lib/train_whistle/twilio.ex
+++ lib/train_whistle/twilio.ex
@@ -6,8 +6,39 @@ defmodule TrainWhistle.Twilio do
 
  @spec send_message(map) :: true | false
   def send_message(to, message \\ []) do
-  data = %{:to => to,  :from => 1, :message => message}
-   true
+  data = %{:to => to,  :from => Application.get_env(:train_whistle, TrainWhistle.Repo)[:twilioNumber], :message => message}
+  response = Twilio.post!("https://api.twilio.com/2010-04-01/Accounts/AC605613134eb9cbd1159a5614dea027bb/Messages.json",     'To=+15005559999&From=+15005550006&Body=Test twilio post',      %{"Authorization": "Basic QUM2MDU2MTMxMzRlYjljYmQxMTU5YTU2MTRkZWEwMjdiYjpkOTMxNzQxZmYyZmEyZGI4MTA4MGY3OTgzNWYyZTM1ZA==",        "Content-Type": "application/x-www-form-urlencoded"}    )
+  #IO.write response
+  true
+  end
+
+  @spec process_request_headers(list) :: list
+  def process_request_headers(headers \\ []) do
+      sid = Application.ge t_env(:train_whistle, TrainWhistle.Repo)[:twilioSid]
+      authCode = Application.get_env(:train_whistle, TrainWhistle.Repo)[:twilioAuth]
+    auth = Base.encode64("#{sid}:#{authCode}")
+    headers
+    |> Keyword.put(:"Content-Type", "application/x-www-form-urlencoded")
+    |> Keyword.put(:"Authorization", "Basic #{auth}")
+  end
+
+   @spec build_url() :: String.t
+  def build_url() do
+    url = "https://api.twilio.com/2010-04-01/Accounts/"
+
+    # Add Account SID segment if not already present
+    url = url <> Application.get_env(:train_whistle, TrainWhistle.Repo)[:twilioSid]
+
+    # Append .json
+    url = url <> ".json"
+
+  end
+
+  @spec to_query_string(list) :: String.t
+  def to_query_string(list) do
+    list
+    |> Enum.map(fn {key, value} -> {camelize(key), value} end)
+    |> URI.encode_query
   end
 
 end
--- test/lib/train_whistle/twilio_test.exs
+++ test/lib/train_whistle/twilio_test.exs
@@ -11,4 +11,14 @@ defmodule TrainWhistle.TwilioTest do
       assert(resp == true)
     end
 
+    test "process_headers/2" do
+        resp = Twilio.process_request_headers()
+        assert(true == Keyword.equal?([Authorization: "Basic QUM2MDU2MTMxMzRlYjljYmQxMTU5YTU2MTRkZWEwMjdiYjpkOTMxNzQxZmYyZmEyZGI4MTA4MGY3OTgzNWYyZTM1ZA==", "Content-Type": "application/x-www-form-urlencoded"], resp))
+    end
+
+    test "url/3" do
+        resp = Twilio.build_url()
+        assert(resp = "https://api.twilio.com/2010-04-01/Accounts/AC605613134eb9cbd1159a5614dea027bb/Messages.json")
+    end
+
 end
