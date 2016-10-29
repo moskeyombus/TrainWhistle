91bc8f844416255883e91dd294edb07874d8f8fa
Drewrey Lupton <drewrey@gmail.com>
2016-10-29 10:51:36 -0400

    fetch twilio number from env var

--- lib/train_whistle/twilio.ex
+++ lib/train_whistle/twilio.ex
@@ -1,12 +1,10 @@
 defmodule TrainWhistle.Twilio do
-  @twilio_number_test "15005550006"
-  @twilio_number "16785155423"
   use HTTPoison.Base
 
   def send_message(to, message) do
     data = %{
       :to => to,
-      :from => @twilio_number,
+      :from => Application.get_env(:train_whistle, TrainWhistle)[:twilio_number],
       :body => message}
     ExTwilio.Message.create(data)
   end
